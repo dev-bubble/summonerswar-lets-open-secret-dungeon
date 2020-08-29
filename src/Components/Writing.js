4b0ba0b648074eda58834beafeaf186d3d96e0bf
jaehyuk-lee225 <jh_lee@heung.me>
2020-08-29 12:35:24 +0900

    Make Headers

--- src/Components/Writing.js
+++ src/Components/Writing.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Header, HeaderInfo } from "./Common/Header";
+import { SelectSD } from "./Common/SelectSD";
 
 export function Writing() {
+  const WritingHeaderInfos = [
+    new HeaderInfo("글작성", "/writing", 3, true),
+    new HeaderInfo("목록", "/", 2, false),
+    // new HeaderInfo("로그인", "/signin", 3, false),
+  ];
   return (
     <div className="App">
+      <Header info={WritingHeaderInfos} />
       <header className="settingHeader">
-        <Link to="/">
-          <div>Back/</div>
-        </Link>
         <div>글 작성</div>
       </header>
-      <body className="settingBody">
+      <div className="settingBody">
         <div>
-          <body className="contents">작성 내용</body>
+          <SelectSD />
+          <div className="contents">
+            <Link to="/">
+              <div className="buttons">작성</div>
+            </Link>
+          </div>
         </div>
-      </body>
+      </div>
       <footer>만든이</footer>
     </div>
   );
