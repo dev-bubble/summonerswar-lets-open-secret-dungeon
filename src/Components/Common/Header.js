74a9d754beaa8cb9172f0c98bc02aefc0e74635a
Kasi <dlwogur5656@gmail.com>
2020-08-29 21:28:01 +0900

    Add Headers

---
+++ src/Components/Common/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export class HeaderInfo {
+  constructor(name, path, letters, active) {
+    this.name = name;
+    this.path = path;
+    this.letter = letters;
+    this.isActive = active;
+  }
+}
+
+export function Header({ info }) {
+  return (
+    <header className="appHeader">
+      <Link to="/">{/* <div className="logo">비던 열자</div> */}</Link>
+      <div className="headerWrapper">
+        <div className="headerContents">
+          {info.map((ele, key) => {
+            return ele.isActive ? (
+              <div key={key} className={"headerContent active l" + ele.letter}>
+                {ele.name}
+              </div>
+            ) : (
+              <Link to={ele.path} key={key}>
+                <div className={"headerContent l" + ele.letter}>{ele.name}</div>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="headerOthers"> 비던 열자▼</div>
+      </div>
+      <div className="bar"> </div>
+    </header>
+  );
+}
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,18 @@
 import React from "react";
+import { Header, HeaderInfo } from "./Common/Header";
+
+export function Main() {
+  const MainHeaderInfos = [
+    new HeaderInfo("목록", "/", 2, true),
+    // new HeaderInfo("조합 정보", "/combination", 4, false),
+    new HeaderInfo("글작성", "/writing", 3, false),
+    new HeaderInfo("설정", "/settings", 2, false),
+  ];
+  return (
+    <div className="App">
+      <header className="appHeader">
+        <div>로고/</div>
+
 import { Link } from "react-router-dom";
 
 export function Main() {
@@ -15,6 +29,19 @@ export function Main() {
           <div>설정</div>
         </Link>
       </header>
+      <body className="appBody">
+        <div>
+          <header className="contentsHeader">필터링 및 정렬</header>
+          <body className="contents">내용</body>
+        </div>
+        <Header info={MainHeaderInfos} />
+        <div className="appBody">
+          <div className="notice">
+            [공지] 조작 된 내용이 있을 수 있으나, 이에 대한 책임은 지지 않음을
+            밝힙니다.
+          </div>
+        </div>
+      </body>
       <div className="appBody">
         <div>
           <header className="contentsHeader">필터링 및 정렬</header>
